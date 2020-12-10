61a2ed2a863693b56ee533b97736590d7173a68d
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2020-12-10 12:55:42 -0700

    Update deploy-local-dev.sh

--- deploy-local-dev.sh
+++ deploy-local-dev.sh
@@ -1,14 +1,12 @@
 #!/bin/sh
 #
-# You can use this script to launch Redis and RabbitMQ on Kubernetes
-# and forward their connections to your local computer. That means
-# you can then work on your worker-server.py and rest-server.py
-# on your local computer rather than pushing to Kubernetes with each change.
-#
-# To kill the port-forward processes us e.g. "ps augxww | grep port-forward"
-# to identify the processes ids
-#
+# This script is used to launch the Redis, MySQL, and RabbitMQ on components using Kubernetes.
+# A separate script is provided for forwarding their connections to your local computer.
+
 kubectl apply -f redis/redis-deployment.yaml
 kubectl apply -f redis/redis-service.yaml
 kubectl apply -f rabbitmq/rabbitmq-deployment.yaml
 kubectl apply -f rabbitmq/rabbitmq-service.yaml
+kubectl apply -f mysql/mysql-deployment.yaml
+kubectl apply -f mysql/mysql-service.yaml
+kubectl apply -f mysql/mysql-volume.yaml
