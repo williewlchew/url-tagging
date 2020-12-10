3583301cfdd507bcb1238fa54c63937013a32f7b
Willie Chew <37847148+williewlchew@users.noreply.github.com>
2020-12-10 12:59:58 -0700

    Update local-portforward.sh

--- local-portforward.sh
+++ local-portforward.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+
 kubectl port-forward --address 0.0.0.0 service/rabbitmq 5672:5672 &
 kubectl port-forward --address 0.0.0.0 service/redis 6379:6379 &
 kubectl port-forward --address 0.0.0.0 service/mysql 3306:3306 &
